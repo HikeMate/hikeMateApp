eb47cd59868bd97141891a14f42b8be137d044fc
mchac15 <matheo.chacon@epfl.ch>
2024-10-06 15:02:15 +0200

    feat: add documentation to both NavigationActions.kt and SideBarNavigation.kt

--- app/src/main/java/ch/hikemate/app/ui/navigation/SideBarNavigation.kt
+++ app/src/main/java/ch/hikemate/app/ui/navigation/SideBarNavigation.kt
@@ -15,13 +15,10 @@ import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.semantics.SemanticsPropertyKey
 import androidx.compose.ui.semantics.semantics
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import ch.hikemate.app.ui.theme.SampleAppTheme
 import kotlinx.coroutines.launch
@@ -34,57 +31,63 @@ const val TEST_TAG_DRAWER_ITEM_PREFIX = "TEST_TAG_DRAWER_ITEM_"
 
 val IsSelectedKey = SemanticsPropertyKey<Boolean>("IsSelected")
 
+/**
+ * Composable function for the sidebar navigation.
+ *
+ * @param onIconSelect Callback function when an icon is selected.
+ * @param tabList List of top-level destinations.
+ * @param selectedItem The currently selected item.
+ */
 @Composable
 fun SideBarNavigation(
     onIconSelect: (TopLevelDestination) -> Unit,
     tabList: List<TopLevelDestination>,
     selectedItem: String
 ) {
-  val drawerState = rememberDrawerState(DrawerValue.Closed)
-  val scope = rememberCoroutineScope()
-  SampleAppTheme {
-    ModalNavigationDrawer(
-        gesturesEnabled = false,
-        drawerState = drawerState,
-        drawerContent = {
-          ModalDrawerSheet(modifier = Modifier.width(180.dp).testTag(TEST_TAG_DRAWER_CONTENT)) {
-            Row {
-              Button(
-                  onClick = { scope.launch { drawerState.close() } },
-                  modifier = Modifier.testTag(TEST_TAG_DRAWER_CLOSE_BUTTON)) {
-                    Icon(Icons.Filled.Menu, contentDescription = "Close")
-                  }
-              Text(TAG, modifier = Modifier.padding(16.dp))
-            }
-            HorizontalDivider()
-            tabList.forEach { tab ->
-              val isSelected = tab.route == selectedItem
-              NavigationDrawerItem(
-                  label = { Text(text = tab.textId) },
-                  selected = isSelected,
-                  onClick = {
-                    onIconSelect(tab)
-                    scope.launch { drawerState.close() }
-                  },
-                  icon = { Icon(tab.icon, contentDescription = tab.textId) },
-                  modifier =
-                      Modifier.testTag(TEST_TAG_DRAWER_ITEM_PREFIX + tab.route).semantics {
-                        set(IsSelectedKey, isSelected)
-                      })
-            }
-          }
-        },
-        content = {
-          IconButton(
-              onClick = { scope.launch { drawerState.open() } },
-              modifier = Modifier.testTag(TEST_TAG_SIDEBAR_BUTTON),
-              content = {
-                Icon(
-                    Icons.Filled.Menu,
-                    contentDescription = "SideBar",
-                )
-              })
-        })
-  }
+    val drawerState = rememberDrawerState(DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+    SampleAppTheme {
+        ModalNavigationDrawer(
+            gesturesEnabled = false,
+            drawerState = drawerState,
+            drawerContent = {
+                ModalDrawerSheet(modifier = Modifier.width(180.dp).testTag(TEST_TAG_DRAWER_CONTENT)) {
+                    Row {
+                        Button(
+                            onClick = { scope.launch { drawerState.close() } },
+                            modifier = Modifier.testTag(TEST_TAG_DRAWER_CLOSE_BUTTON)) {
+                            Icon(Icons.Filled.Menu, contentDescription = "Close")
+                        }
+                        Text(TAG, modifier = Modifier.padding(16.dp))
+                    }
+                    HorizontalDivider()
+                    tabList.forEach { tab ->
+                        val isSelected = tab.route == selectedItem
+                        NavigationDrawerItem(
+                            label = { Text(text = tab.textId) },
+                            selected = isSelected,
+                            onClick = {
+                                onIconSelect(tab)
+                                scope.launch { drawerState.close() }
+                            },
+                            icon = { Icon(tab.icon, contentDescription = tab.textId) },
+                            modifier =
+                            Modifier.testTag(TEST_TAG_DRAWER_ITEM_PREFIX + tab.route).semantics {
+                                set(IsSelectedKey, isSelected)
+                            })
+                    }
+                }
+            },
+            content = {
+                IconButton(
+                    onClick = { scope.launch { drawerState.open() } },
+                    modifier = Modifier.testTag(TEST_TAG_SIDEBAR_BUTTON),
+                    content = {
+                        Icon(
+                            Icons.Filled.Menu,
+                            contentDescription = "SideBar",
+                        )
+                    })
+            })
+    }
 }
-
