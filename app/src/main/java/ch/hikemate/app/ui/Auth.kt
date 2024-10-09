468db3e19b9191029f46391af0c75645d342030b
DavidHolzwarth <david.holzwarth@tum.de>
2024-10-09 13:48:22 +0200

    feat(auth): Implement firebase authentication functionality

--- app/src/main/java/ch/hikemate/app/ui/Auth.kt
+++ app/src/main/java/ch/hikemate/app/ui/Auth.kt
@@ -1,10 +1,6 @@
 package ch.hikemate.app.ui
 
-import android.os.Build
 import android.util.Log
-import androidx.annotation.RequiresApi
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -14,14 +10,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import androidx.credentials.CredentialManager
-import androidx.credentials.GetCredentialResponse
-import androidx.credentials.exceptions.GetCredentialException
 import ch.hikemate.app.R
 import com.google.android.libraries.identity.googleid.GetGoogleIdOption
 import com.google.firebase.auth.GoogleAuthProvider
@@ -33,92 +25,83 @@ import kotlinx.coroutines.launch
 @Composable
 fun SignInButton(
     onAuthComplete: () -> Unit,
-    onErrorAction: (GetCredentialException) -> Unit
+    onErrorAction: (Throwable) -> Unit,
+    credentialManager: CredentialManager = CredentialManager.create(LocalContext.current)
 ) {
 
-    var user by remember { mutableStateOf(Firebase.auth.currentUser) }
-    val token = stringResource(R.string.default_web_client_id)
-    val context = LocalContext.current
-    val coroutineScope: CoroutineScope = rememberCoroutineScope()
+  var user by remember { mutableStateOf(Firebase.auth.currentUser) }
+  val token = stringResource(R.string.default_web_client_id)
+  val context = LocalContext.current
+  val coroutineScope: CoroutineScope = rememberCoroutineScope()
 
-    if (user == null) {
-        Button(
-            modifier = Modifier.testTag("signInButton"),
-            onClick = {
+  if (user == null) {
+    Button(
+        modifier = Modifier.testTag("signInButton"),
+        onClick = {
+          val auth = Firebase.auth
 
-                val credentialManager = CredentialManager.create(context)
-                val auth = Firebase.auth
-                val googleIdOption: GetGoogleIdOption = GetGoogleIdOption.Builder()
-                    .setFilterByAuthorizedAccounts(true)
-                    .setServerClientId(token)
-                    .setAutoSelectEnabled(true)
-                    .build()
-                val request: androidx.credentials.GetCredentialRequest =
-                    androidx.credentials.GetCredentialRequest.Builder()
-                        .addCredentialOption(googleIdOption)
-                        .build()
+          val googleIdOption: GetGoogleIdOption =
+              GetGoogleIdOption.Builder()
+                  .setFilterByAuthorizedAccounts(true)
+                  .setServerClientId(token)
+                  .setAutoSelectEnabled(true)
+                  .build()
 
-                coroutineScope.launch {
-                    try {
-                        val result = credentialManager.getCredential(
-                            request = request,
-                            context = context,
-                        )
+          val request: androidx.credentials.GetCredentialRequest =
+              androidx.credentials.GetCredentialRequest.Builder()
+                  .addCredentialOption(googleIdOption)
+                  .build()
 
-                        val firebaseCredential = GoogleAuthProvider.getCredential(
-                            result.credential.data.getString("com.google.android.libraries.identity.googleid.BUNDLE_KEY_ID_TOKEN")!!,
-                            null
-                        )
+          coroutineScope.launch {
+            try {
+              val result =
+                  credentialManager.getCredential(
+                      request = request,
+                      context = context,
+                  )
+              val firebaseCredential =
+                  GoogleAuthProvider.getCredential(
+                      result.credential.data.getString(
+                          "com.google.android.libraries.identity.googleid.BUNDLE_KEY_ID_TOKEN")!!,
+                      null)
 
-                        auth.signInWithCredential(firebaseCredential)
-                            .addOnCompleteListener { task ->
-                                if (task.isSuccessful) {
-                                    Log.d("SignInButton", "signInWithCredential:success")
-                                    user = auth.currentUser
-                                } else {
-                                    Log.w(
-                                        "SignInButton",
-                                        "signInWithCredential:failure",
-                                        task.exception
-                                    )
-                                }
-                            }
-                    } catch (e: GetCredentialException) {
-                        onErrorAction(e)
-                        Log.d(
-                            "SignInButton", "Login error: ${e.message}"
-                        )
-                    }
+              auth.signInWithCredential(firebaseCredential).addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                  user = auth.currentUser
+                } else {
+                  Log.w("SignInButton", "signInWithCredential:failure", task.exception)
+                  onErrorAction(task.exception ?: Throwable("Unknown error"))
                 }
-            }) { Text("Sign in with Google") }
-    } else {
-        onAuthComplete()
-    }
+              }
+            } catch (e: Throwable) {
+              onErrorAction(e)
+              Log.d("SignInButton", "Login error: ${e.message}")
+            }
+          }
+        }) {
+          Text("Sign in with Google")
+        }
+  } else {
+    onAuthComplete()
+  }
 }
 
-
-
 @Composable
-fun SignOutButton(
-    onSignOutComplete: () -> Unit
-) {
+fun SignOutButton(onSignOutComplete: () -> Unit) {
 
-    var user by remember { mutableStateOf(Firebase.auth.currentUser) }
+  var user by remember { mutableStateOf(Firebase.auth.currentUser) }
 
-    if (user != null) {
-        Button(
-            onClick = {
-                // Call Firebase sign out
-                Firebase.auth.signOut()
-                user = null // Update the state to reflect the logout
-
-            },
-            modifier = Modifier
-                .testTag("signOutButton")
-        ) {
-            Text("Sign out")
+  if (user != null) {
+    Button(
+        onClick = {
+          // Call Firebase sign out
+          Firebase.auth.signOut()
+          user = null // Update the state to reflect the logout
+        },
+        modifier = Modifier.testTag("signOutButton")) {
+          Text("Sign out")
         }
-    } else {
-        onSignOutComplete()
-    }
+  } else {
+    onSignOutComplete()
+  }
 }
--- app/src/main/java/ch/hikemate/app/ui/SignIn.kt
+++ app/src/main/java/ch/hikemate/app/ui/SignIn.kt
@@ -1,2 +1,1 @@
 package ch.hikemate.app.ui
-
