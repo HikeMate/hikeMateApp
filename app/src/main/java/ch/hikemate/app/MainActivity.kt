c3d86b96af2c2cdbc30bb94bc8729e5145a410e8
rBergCode <rberguerand@gmail.com>
2024-10-10 09:26:08 +0200

    feat(auth): implement the sign in screen
    Implemented the sign in screen in accordance with the Figma design.

--- app/src/main/java/ch/hikemate/app/MainActivity.kt
+++ app/src/main/java/ch/hikemate/app/MainActivity.kt
@@ -4,40 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.semantics.semantics
-import androidx.compose.ui.semantics.testTag
-import androidx.compose.ui.tooling.preview.Preview
-import ch.hikemate.app.resources.C
+import ch.hikemate.app.ui.auth.SignInScreen
 import ch.hikemate.app.ui.theme.SampleAppTheme
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContent {
-      SampleAppTheme {
-        // A surface container using the 'background' color from the theme
-        Surface(
-            modifier = Modifier.fillMaxSize().semantics { testTag = C.Tag.main_screen_container },
-            color = MaterialTheme.colorScheme.background) {
-              Greeting("Android")
-            }
-      }
-    }
+      setContent { SampleAppTheme { Surface(modifier = Modifier.fillMaxSize()) { SignInScreen() } } }
   }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-  Text(text = "Hello $name!", modifier = modifier.semantics { testTag = C.Tag.greeting })
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-  SampleAppTheme { Greeting("Android") }
 }
---
+++ app/src/main/java/ch/hikemate/app/ui/auth/SignInScreen.kt
@@ -0,0 +1,156 @@
+package ch.hikemate.app.ui.auth
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.blur
+import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import ch.hikemate.app.R
+import ch.hikemate.app.ui.components.AppIcon
+import ch.hikemate.app.ui.theme.SampleAppTheme
+
+/**
+ * A composable function to display the sign in screen
+ */
+@Composable
+fun SignInScreen() {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        content = { padding ->
+            Box(
+                modifier = Modifier
+                    .fillMaxSize(),
+            ) {
+                // I created a box to make the background image take a little more space
+                // than the screen size, so that the blur effect doesn't show white edges
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .clipToBounds()
+                        .scale(1.1f),
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.sign_in_background),
+                        contentDescription = "Background Image",
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .blur(10.dp),
+                        contentScale = ContentScale.Crop,
+                    )
+                }
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(padding),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.SpaceAround,
+                ) {
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                    ) {
+                        // App Logo Image
+                        AppIcon(200.dp)
+                        Spacer(modifier = Modifier.height(16.dp))
+
+                        // App name Text
+                        Text(
+                            modifier = Modifier.testTag("appNameText"),
+                            text = "HikeMate",
+                            style = TextStyle(
+                                color = Color.White,
+                                fontFamily = FontFamily(
+                                    Font(
+                                        R.font.kaushan_script,
+                                        FontWeight.Bold
+                                    )
+                                ),
+                                fontSize = 60.sp,
+                                fontWeight = FontWeight.Bold,
+                            ),
+                        )
+                    }
+
+                    SignInWithGoogleButton { /* Handle sign in click here */ }
+                }
+            }
+        },
+    )
+}
+
+/**
+ * A composable function to display the sign in with Google button
+ *
+ * @param onSignInClick A lambda function to handle the sign in click event
+ */
+@Composable
+fun SignInWithGoogleButton(onSignInClick: () -> Unit) {
+    Button(
+        onClick = onSignInClick,
+        colors = ButtonDefaults.buttonColors(containerColor = Color.White),
+        shape = RoundedCornerShape(50),
+        modifier =
+        Modifier
+            .padding(8.dp)
+            .height(48.dp)
+            .border(
+                width = 3.dp,
+                color = Color(0xFF3B9DE8),
+                shape = RoundedCornerShape(size = 32.dp)
+            )
+            .testTag("loginButton")
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            // Load the Google logo from resources
+            Image(
+                painter =
+                painterResource(id = R.drawable.google_logo),
+                contentDescription = "Google Logo",
+                modifier = Modifier
+                    .size(30.dp)
+                    .padding(end = 8.dp)
+            )
+
+            // Text for the button
+            Text(
+                text = "Sign In with Google",
+                color = Color.Black, // Text color
+                fontSize = 18.sp, // Font size
+                fontWeight = FontWeight.Bold,
+            )
+        }
+    }
+}
