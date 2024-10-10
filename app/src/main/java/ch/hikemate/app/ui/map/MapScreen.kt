db3a1a211472af5eb2db0230dd50b3140b3788ff
Maël Imhof <contact@mael.im>
2024-10-10 16:47:30 +0200

    feat(map): load routes from view model
    Routes displayed in the hikes list of the map are now loaded from the view model. Every time the user moves the map, the displayed area is updated in the view model to update the hikes as well.
    
    Still need to have a cooldown for API requests, since the updates can be quite frequent right now.

--- app/src/main/java/ch/hikemate/app/ui/map/MapScreen.kt
+++ app/src/main/java/ch/hikemate/app/ui/map/MapScreen.kt
@@ -1,5 +1,6 @@
 package ch.hikemate.app.ui.map
 
+import android.util.Log
 import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
@@ -8,6 +9,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
@@ -18,6 +20,7 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -26,12 +29,16 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
+import ch.hikemate.app.model.map.ListOfHikeRoutesViewModel
 import org.osmdroid.config.Configuration
+import org.osmdroid.events.MapListener
+import org.osmdroid.events.ScrollEvent
+import org.osmdroid.events.ZoomEvent
 import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.MapView
 
 @Composable
-fun MapScreen() {
+fun MapScreen(hikingRoutesViewModel: ListOfHikeRoutesViewModel) {
   val context = LocalContext.current
   // Avoid re-creating the MapView on every recomposition
   val mapView = remember { MapView(context) }
@@ -50,6 +57,21 @@ fun MapScreen() {
     controller.setCenter(GeoPoint(46.5, 6.6))
     // Enable touch-controls such as pinch to zoom
     setMultiTouchControls(true)
+    // Update hiking routes every time the user moves the map
+    // TODO : Those updates could be quite frequent, have a cooldown to avoid sending a request each time?
+    addMapListener(object : MapListener {
+      override fun onScroll(event: ScrollEvent?): Boolean {
+        Log.d("MapScreen", "onScroll")
+        hikingRoutesViewModel.setArea(mapView.boundingBox)
+        return true
+      }
+
+      override fun onZoom(event: ZoomEvent?): Boolean {
+        Log.d("MapScreen", "onZoom")
+        hikingRoutesViewModel.setArea(mapView.boundingBox)
+        return true
+      }
+    })
   }
 
   Box(modifier = Modifier.fillMaxSize()) {
@@ -75,33 +97,41 @@ fun MapScreen() {
       Icon(Icons.Default.Menu, contentDescription = "Menu", tint = Color.Black)
     }
 
-    CollapsibleHikesList()
+    CollapsibleHikesList(hikingRoutesViewModel)
   }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun CollapsibleHikesList() {
+fun CollapsibleHikesList(hikingRoutesViewModel: ListOfHikeRoutesViewModel) {
   val scaffoldState = rememberBottomSheetScaffoldState()
+  val routes = hikingRoutesViewModel.hikeRoutes.collectAsState()
 
   BottomSheetScaffold(
     scaffoldState = scaffoldState,
     sheetContent = {
-      // TODO : Maybe use a LazyColumn here instead?
-      Column (
-        modifier = Modifier
-          .fillMaxSize()
-          .padding(16.dp)
+
+      Column(modifier = Modifier
+        .fillMaxSize()
+        .padding(16.dp)
       ) {
-        // TODO : Import hikes from viewmodel
         Text("Hikes")
         Spacer(modifier = Modifier.height(10.dp))
-        Text("Dents du Midi - 834m")
-        Text("Dents du quatre heures - 412m")
-        Text("Dents du Minuit - 1356m")
+
+        LazyColumn(
+          modifier = Modifier
+            .fillMaxSize()
+        ) {
+          items(routes.value.size) { index: Int ->
+            Text(routes.value[index])
+          }
+        }
       }
     },
     // TODO : Adjust this value and the behavior of the sheet
     sheetPeekHeight = 100.dp
   ) { }
+
+  // TODO : Is this the right place to call getRoutes()?
+  hikingRoutesViewModel.getRoutes()
 }
