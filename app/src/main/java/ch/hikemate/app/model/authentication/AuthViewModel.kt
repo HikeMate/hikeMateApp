ed6d53358cb959898a85da95628503738a51eee6
DavidHolzwarth <david.holzwarth@tum.de>
2024-10-14 10:50:21 +0200

    feat(auth): add viewModel for firebase authentication

--- app/src/main/java/ch/hikemate/app/model/authentication/AuthViewModel.kt
+++ app/src/main/java/ch/hikemate/app/model/authentication/AuthViewModel.kt
@@ -10,26 +10,21 @@ import kotlinx.coroutines.flow.StateFlow
 class AuthViewModel(private val repository: FirebaseAuthRepository) {
 
   private val _currentUser = MutableStateFlow(FirebaseAuth.getInstance().currentUser)
-  val currentUser: StateFlow<FirebaseUser?> get() = _currentUser
+  val currentUser: StateFlow<FirebaseUser?>
+    get() = _currentUser
 
-  /**
-   *
-   */
-  fun signInWithGoogle(
-    coroutineScope: CoroutineScope,
-    context: Context
-  ) {
+  /**  */
+  fun signInWithGoogle(coroutineScope: CoroutineScope, context: Context) {
     repository.signInWithGoogle(
-      onSuccess = { user: FirebaseUser? -> _currentUser.value = user },
-      onErrorAction = { },
-      context = context,
-      coroutineScope = coroutineScope
-    )
+        onSuccess = { user: FirebaseUser? -> _currentUser.value = user },
+        onErrorAction = {},
+        context = context,
+        coroutineScope = coroutineScope)
   }
 
   fun signOut() {
     repository.signOut(
-      onSuccess = { _currentUser.value = null },
+        onSuccess = { _currentUser.value = null },
     )
   }
 }
