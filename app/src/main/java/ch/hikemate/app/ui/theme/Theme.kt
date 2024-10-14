37c7635ac3766a243411acbbf2ca7329f261e115
Wylwi <wylwi.fr@gmail.com>
2024-10-14 18:14:33 +0200

    chore: remove template files and rename to real project name

--- app/src/main/java/ch/hikemate/app/ui/theme/Theme.kt
+++ app/src/main/java/ch/hikemate/app/ui/theme/Theme.kt
@@ -10,7 +10,6 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
@@ -34,7 +33,7 @@ private val LightColorScheme =
         )
 
 @Composable
-fun SampleAppTheme(
+fun HikeMateTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
@@ -53,7 +52,6 @@ fun SampleAppTheme(
   if (!view.isInEditMode) {
     SideEffect {
       val window = (view.context as Activity).window
-      window.statusBarColor = colorScheme.primary.toArgb()
       WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
     }
   }
