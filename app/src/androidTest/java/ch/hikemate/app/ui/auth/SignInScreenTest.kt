5d7ab6aef462726539cc2e042d20a0382586a7df
Wylwi <wylwi.fr@gmail.com>
2024-10-17 22:29:13 +0200

    test: add a test to check the error handling of SignInScreen

--- app/src/androidTest/java/ch/hikemate/app/ui/auth/SignInScreenTest.kt
+++ app/src/androidTest/java/ch/hikemate/app/ui/auth/SignInScreenTest.kt
@@ -6,9 +6,13 @@ import androidx.compose.ui.test.assertTextEquals
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
+import androidx.credentials.exceptions.NoCredentialException
+import androidx.navigation.compose.rememberNavController
 import androidx.test.espresso.intent.Intents
 import ch.hikemate.app.model.authentication.AuthViewModel
+import ch.hikemate.app.ui.navigation.NavigationActions
 import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
+import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
 import org.junit.After
@@ -24,7 +28,9 @@ class SignInScreenTest : TestCase() {
   @Before
   fun setUp() {
     Intents.init()
-    composeTestRule.setContent { SignInScreen(mockk(), mockViewModel) }
+    composeTestRule.setContent {
+      SignInScreen(NavigationActions(rememberNavController()), mockViewModel)
+    }
   }
 
   // Release Intents after each test
@@ -40,15 +46,32 @@ class SignInScreenTest : TestCase() {
     composeTestRule.onNodeWithTag("appNameText").assertIsDisplayed()
     composeTestRule.onNodeWithTag("appNameText").assertTextEquals("HikeMate")
 
-    composeTestRule.onNodeWithTag("loginButton").assertIsDisplayed()
-    composeTestRule.onNodeWithTag("loginButton").assertHasClickAction()
+    composeTestRule.onNodeWithTag(TEST_TAG_LOGIN_BUTTON).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(TEST_TAG_LOGIN_BUTTON).assertHasClickAction()
   }
 
   @Test
   fun loginButtonCallsSignIn() {
-    composeTestRule.onNodeWithTag("loginButton").performClick()
+    composeTestRule.onNodeWithTag(TEST_TAG_LOGIN_BUTTON).performClick()
 
     // Verify that the signIn function was called
     verify { mockViewModel.signInWithGoogle(any(), any(), any(), any()) }
+  }
+
+  @Test
+  @Suppress("UNCHECKED_CAST")
+  fun signInWithError() {
+    // Mock the signIn function to return an error
+    every { mockViewModel.signInWithGoogle(any(), any(), any(), any()) } answers
+        {
+          val onError = args[3] as (NoCredentialException) -> Unit
+          onError(NoCredentialException("No credentials found"))
+        }
+
+    // Click the login button
+    composeTestRule.onNodeWithTag(TEST_TAG_LOGIN_BUTTON).performClick()
+
+    // The button should still be displayed
+    composeTestRule.onNodeWithTag(TEST_TAG_LOGIN_BUTTON).assertIsDisplayed()
   }
 }
