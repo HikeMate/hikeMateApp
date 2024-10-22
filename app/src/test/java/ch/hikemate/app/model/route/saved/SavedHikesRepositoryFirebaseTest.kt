09bf106d6d938540e70e5a2ad2aec94ac0eab7fa
Maël Imhof <contact@mael.im>
2024-10-22 17:03:37 +0200

    test(saved-hikes): dummy tests for repository
    Implemented dummy tests for the saved hikes repository. It is not properly implemented yet, so there's not much to test.
    
    Tests will be updated when the repository is implemented correctly with Firebase interaction.

---
+++ app/src/test/java/ch/hikemate/app/model/route/saved/SavedHikesRepositoryFirebaseTest.kt
@@ -0,0 +1,59 @@
+package ch.hikemate.app.model.route.saved
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+
+class SavedHikesRepositoryFirebaseTest {
+  private lateinit var savedHikesRepositoryFirebase: SavedHikesRepositoryFirebase
+
+  @OptIn(ExperimentalCoroutinesApi::class)
+  @Before
+  fun setUp() {
+    Dispatchers.setMain(UnconfinedTestDispatcher())
+
+    savedHikesRepositoryFirebase = SavedHikesRepositoryFirebase()
+  }
+
+  @Test
+  fun loadSavedHikesReturnsDummyData() = runTest {
+    // When
+    val savedHikes = savedHikesRepositoryFirebase.loadSavedHikes()
+
+    // Then
+    assertEquals(3, savedHikes.size)
+    assertEquals("1", savedHikes[0].id)
+    assertEquals("Hike 1", savedHikes[0].name)
+    assertEquals("2", savedHikes[1].id)
+    assertEquals("Hike 2", savedHikes[1].name)
+    assertEquals("3", savedHikes[2].id)
+    assertEquals("Hike 3", savedHikes[2].name)
+  }
+
+  @Test
+  fun addSavedHikeDoesNothing() = runTest {
+    // Add saved hike is a placeholder for now, it does nothing under the hood
+
+    // When
+    savedHikesRepositoryFirebase.addSavedHike(SavedHike("4", "Hike 4", null))
+
+    // Then
+    assert(true)
+  }
+
+  @Test
+  fun removeSavedHikeDoesNothing() = runTest {
+    // Remove saved hike is a placeholder for now, it does nothing under the hood
+
+    // When
+    savedHikesRepositoryFirebase.removeSavedHike(SavedHike("4", "Hike 4", null))
+
+    // Then
+    assert(true)
+  }
+}
