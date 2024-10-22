a29d3d5469d0e7213ee546c9f0d7cc62a620e058
mchac15 <matheo.chacon@epfl.ch>
2024-10-22 13:54:55 +0200

    fix: run ktmft format

--- app/src/main/java/ch/hikemate/app/model/elevation/ElevationServiceRepository.kt
+++ app/src/main/java/ch/hikemate/app/model/elevation/ElevationServiceRepository.kt
@@ -21,8 +21,7 @@ private const val CONTENT_TYPE_HEADER = "Content-Type"
 private val JSON_MEDIA_TYPE = "application/json; charset=utf-8".toMediaType()
 
 /** Serializable data class for the response from the Elevation API */
-@Serializable
-data class ElevationResponse(val results: List<ElevationResult>)
+@Serializable data class ElevationResponse(val results: List<ElevationResult>)
 
 /** Serializable data class for the response from the Elevation API */
 @Serializable
@@ -33,104 +32,102 @@ data class ElevationResult(val latitude: Double, val longitude: Double, val elev
  */
 class ElevationServiceRepository(private val client: OkHttpClient) : ElevationService {
 
-    // Cache for the elevation data, indexed by hike ID
-    private val cache = mutableMapOf<Long, List<Double>>()
+  // Cache for the elevation data, indexed by hike ID
+  private val cache = mutableMapOf<Long, List<Double>>()
 
-    /**
-     * Get the size of the cache
-     *
-     * @return The size of the cache
-     */
-    fun getCacheSize(): Int {
-        return cache.size
+  /**
+   * Get the size of the cache
+   *
+   * @return The size of the cache
+   */
+  fun getCacheSize(): Int {
+    return cache.size
+  }
+
+  override fun getElevation(
+      coordinates: List<LatLong>,
+      hikeID: Long,
+      onSuccess: (List<Double>) -> Unit,
+      onFailure: (Exception) -> Unit
+  ) {
+    if (coordinates.isEmpty()) {
+      onFailure(Exception("No coordinates provided"))
+      return
     }
-
-    override fun getElevation(
-        coordinates: List<LatLong>,
-        hikeID: Long,
-        onSuccess: (List<Double>) -> Unit,
-        onFailure: (Exception) -> Unit
-    ) {
-        if (coordinates.isEmpty()) {
-            onFailure(Exception("No coordinates provided"))
-            return
-        }
-        // If the cache already has the elevation data for this hike, call onSuccess
-        for (mutableEntry in cache) {
-            if (mutableEntry.key == hikeID) {
-                onSuccess(mutableEntry.value)
-                return
+    // If the cache already has the elevation data for this hike, call onSuccess
+    for (mutableEntry in cache) {
+      if (mutableEntry.key == hikeID) {
+        onSuccess(mutableEntry.value)
+        return
+      }
+    }
+    // Create the JSON body for the request
+    val jsonBody = buildJsonObject {
+      // The major object
+      put(
+          LOCATION_KEY,
+          buildJsonArray {
+            coordinates.forEach { coordinate ->
+              add(
+                  // Object with the location
+                  buildJsonObject {
+                    put(LATITUDE_KEY, coordinate.lat)
+                    put(LONGITUDE_KEY, coordinate.lon)
+                  })
             }
-        }
-        // Create the JSON body for the request
-        val jsonBody = buildJsonObject {
-            // The major object
-            put(
-                LOCATION_KEY,
-                buildJsonArray {
-                    coordinates.forEach { coordinate ->
-                        add(
-                            // Object with the location
-                            buildJsonObject {
-                                put(LATITUDE_KEY, coordinate.lat)
-                                put(LONGITUDE_KEY, coordinate.lon)
-                            })
-                    }
-                })
-        }
-
-        val body =
-            RequestBody.create(JSON_MEDIA_TYPE, jsonBody.toString())
-
-        val requestBuilder =
-            okhttp3.Request.Builder()
-                .url(BASE_URL)
-                .post(body)
-                .addHeader(ACCEPT_HEADER, APPLICATION_JSON)
-                .addHeader(CONTENT_TYPE_HEADER, APPLICATION_JSON)
-                .build()
-
-        // Callback for the network request, it also updates the cache
-        val onSuccessWithCache: (List<Double>) -> Unit = {
-            cache[hikeID] = it
-            onSuccess(it)
-        }
-        client.newCall(requestBuilder)
-            .enqueue(ElevationServiceCallback(onSuccessWithCache, onFailure))
+          })
     }
 
-    /**
-     * A callback for the network request to the Elevation API. This callback will parse the response
-     */
-    private inner class ElevationServiceCallback(
-        private val onSuccess: (List<Double>) -> Unit,
-        private val onFailure: (Exception) -> Unit
-    ) : okhttp3.Callback {
+    val body = RequestBody.create(JSON_MEDIA_TYPE, jsonBody.toString())
 
-        override fun onFailure(call: okhttp3.Call, e: IOException) {
-            onFailure(e)
-        }
+    val requestBuilder =
+        okhttp3.Request.Builder()
+            .url(BASE_URL)
+            .post(body)
+            .addHeader(ACCEPT_HEADER, APPLICATION_JSON)
+            .addHeader(CONTENT_TYPE_HEADER, APPLICATION_JSON)
+            .build()
 
-        override fun onResponse(call: okhttp3.Call, response: okhttp3.Response) {
-            if (response.isSuccessful) {
-                val body = response.body?.string()
-                print(body)
-
-                if (body == null) {
-                    onFailure(Exception("Failed to get elevation"))
-                    return
-                }
-
-                // This function decodes the JSON response into a ElevationResponse which is a List of
-                // ElevationResult
-                val elevationResponse = decodeFromString<ElevationResponse>(body)
-
-                // Call the onSuccess callback with the list of elevations
-                onSuccess(elevationResponse.results.map { it.elevation })
-            } else {
-
-                onFailure(Exception("Failed to get elevation"))
-            }
-        }
+    // Callback for the network request, it also updates the cache
+    val onSuccessWithCache: (List<Double>) -> Unit = {
+      cache[hikeID] = it
+      onSuccess(it)
     }
+    client.newCall(requestBuilder).enqueue(ElevationServiceCallback(onSuccessWithCache, onFailure))
+  }
+
+  /**
+   * A callback for the network request to the Elevation API. This callback will parse the response
+   */
+  private inner class ElevationServiceCallback(
+      private val onSuccess: (List<Double>) -> Unit,
+      private val onFailure: (Exception) -> Unit
+  ) : okhttp3.Callback {
+
+    override fun onFailure(call: okhttp3.Call, e: IOException) {
+      onFailure(e)
+    }
+
+    override fun onResponse(call: okhttp3.Call, response: okhttp3.Response) {
+      if (response.isSuccessful) {
+        val body = response.body?.string()
+        print(body)
+
+        if (body == null) {
+          onFailure(Exception("Failed to get elevation"))
+          return
+        }
+
+        // This function decodes the JSON response into a ElevationResponse which is a List of
+        // ElevationResult
+        val elevationResponse = decodeFromString<ElevationResponse>(body)
+
+        // Call the onSuccess callback with the list of elevations
+        onSuccess(elevationResponse.results.map { it.elevation })
+      } else {
+
+        onFailure(Exception("Failed to get elevation"))
+      }
+    }
+  }
 }
