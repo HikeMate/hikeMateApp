d86c02f8c4344846e44d3e08d7c54b7ecd9a4913
Maël Imhof <mael.imhof@epfl.ch>
2024-10-24 19:09:40 +0200

    feat(saved-hikes): add repository and view model (#66)
    Add the view model as well as a skeleton for the repository of the Saved Hikes user story.
    
    This includes getting a list of the hikes the current user has saved, adding a saved hike and removing an existing one.
    
    Only the view model implementation and a dummy repository example are provided in this commit. The actual screen (view) and the proper repository implementation that uses firebase (model) are yet to be implemented.
    
    See #66 (the PR) and #63 (the issue/task)

---
+++ app/src/test/java/ch/hikemate/app/model/route/saved/SavedHikesRepositoryDummyTest.kt
@@ -0,0 +1,75 @@
+package ch.hikemate.app.model.route.saved
+
+import kotlin.time.Duration.Companion.seconds
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+
+class SavedHikesRepositoryDummyTest {
+  private lateinit var savedHikesRepositoryDummy: SavedHikesRepositoryDummy
+
+  @OptIn(ExperimentalCoroutinesApi::class)
+  @Before
+  fun setUp() {
+    Dispatchers.setMain(UnconfinedTestDispatcher())
+
+    savedHikesRepositoryDummy = SavedHikesRepositoryDummy()
+  }
+
+  @Test
+  fun loadSavedHikesReturnInitialDummyData() =
+      runTest(timeout = 5.seconds) {
+        // When
+        val savedHikes = savedHikesRepositoryDummy.loadSavedHikes()
+
+        // Then
+        assertEquals(3, savedHikes.size)
+        assertEquals("1", savedHikes[0].id)
+        assertEquals("Hike 1", savedHikes[0].name)
+        assertEquals("2", savedHikes[1].id)
+        assertEquals("Hike 2", savedHikes[1].name)
+        assertEquals("3", savedHikes[2].id)
+        assertEquals("Hike 3", savedHikes[2].name)
+      }
+
+  @Test
+  fun addSavedHikeSavesTheHike() =
+      runTest(timeout = 5.seconds) {
+        // Given
+        val hike = SavedHike("4", "Hike 4", null)
+
+        // Before
+        assertEquals(3, savedHikesRepositoryDummy.loadSavedHikes().size)
+        assert(!savedHikesRepositoryDummy.loadSavedHikes().contains(hike))
+
+        // When
+        savedHikesRepositoryDummy.addSavedHike(hike)
+
+        // Then
+        assertEquals(4, savedHikesRepositoryDummy.loadSavedHikes().size)
+        assert(savedHikesRepositoryDummy.loadSavedHikes().contains(hike))
+      }
+
+  @Test
+  fun removeSavedHikeUnSavesTheHike() =
+      runTest(timeout = 5.seconds) {
+        // Given
+        val hike = SavedHike("3", "Hike 3", null)
+
+        // Before
+        assertEquals(3, savedHikesRepositoryDummy.loadSavedHikes().size)
+        assert(savedHikesRepositoryDummy.loadSavedHikes().contains(hike))
+
+        // When
+        savedHikesRepositoryDummy.removeSavedHike(hike)
+
+        // Then
+        assertEquals(2, savedHikesRepositoryDummy.loadSavedHikes().size)
+        assert(!savedHikesRepositoryDummy.loadSavedHikes().contains(hike))
+      }
+}
