41ded5471ee25f2410232569fb044f8ff4d1b30b
Maël Imhof <contact@mael.im>
2024-10-27 13:02:38 +0100

    feat(saved-hikes): add more dummy saved hikes
    To test the Saved Hikes screen properly, there needs to be some saved hikes with a planned date. I add some to SavedHikesRepositoryDummy using the newly created util function that creates a timestamp from a year, month and day.

--- app/src/main/java/ch/hikemate/app/model/route/saved/SavedHikesRepositoryDummy.kt
+++ app/src/main/java/ch/hikemate/app/model/route/saved/SavedHikesRepositoryDummy.kt
@@ -1,11 +1,27 @@
 package ch.hikemate.app.model.route.saved
 
+import ch.hikemate.app.utils.from
+import com.google.firebase.Timestamp
+import java.time.LocalDate
+
 class SavedHikesRepositoryDummy : SavedHikesRepository {
   private val savedHikes =
       mutableListOf(
           SavedHike("1", "Hike 1", null),
           SavedHike("2", "Hike 2", null),
-          SavedHike("3", "Hike 3", null))
+          SavedHike("3", "Hike 3", null),
+          SavedHike(
+              "4",
+              "Hike 4",
+              Timestamp.from(LocalDate.now().year, LocalDate.now().monthValue + 1, 1)),
+          SavedHike(
+              "5",
+              "Hike 5",
+              Timestamp.from(LocalDate.now().year, LocalDate.now().monthValue + 1, 2)),
+          SavedHike(
+              "6",
+              "Hike 6",
+              Timestamp.from(LocalDate.now().year, LocalDate.now().monthValue + 1, 3)))
 
   override suspend fun loadSavedHikes(): List<SavedHike> {
     return savedHikes
