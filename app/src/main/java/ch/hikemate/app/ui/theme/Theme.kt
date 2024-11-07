11c7a9c68d8e0fccb93f11170c6c3efe80cb7c1e
Rémy <60262490+rBergCode@users.noreply.github.com>
2024-11-07 21:52:56 +0100

    fix(ui): fix status bar color (#121)

--- app/src/main/java/ch/hikemate/app/ui/theme/Theme.kt
+++ app/src/main/java/ch/hikemate/app/ui/theme/Theme.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
@@ -34,7 +35,9 @@ fun HikeMateTheme(
   if (!view.isInEditMode) {
     SideEffect {
       val window = (view.context as Activity).window
-      WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+      window.statusBarColor = colorScheme.background.toArgb()
+      window.navigationBarColor = colorScheme.background.toArgb()
+      WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
     }
   }
 
