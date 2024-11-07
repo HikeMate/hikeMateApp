d0dedb14795cdee73256f1e0414e93f402036b63
David Holzwarth <88078941+davidholzwarth@users.noreply.github.com>
2024-11-07 20:06:02 +0100

    feat(auth): merge firebase auth with signin composable
    * fix: try to fix the authentication error
    
    * fix: try to fix the authentication error
    
    * fix: change type of googleIdOption
    
    * chore: change mainactivity temporarily to enable teammates to test the new implementation
    
    * fix: add feature that catches if the device has no google account yet
    
    * fix: adjust authviewmodel according to last commit
    
    * fix: add feature that automatically triggers sign in after google acc has been added to device
    
    * feat: add test for fixed firebase login and adapted old tests
    
    * chore: clean up mainactivity
    
    * chore: clean up code
    
    * fix: change noCredentialExeption catch to work again
    
    * feat: add sign in functionality to the sign in button
    
    * feat: add sign in functionality to the sign in button
    
    * fix: start fixing tests
    
    * feat: implement new tests for signinbutton UI
    
    * feat: one more signinscreen test for after the sign in was successful
    
    * chore: ktfmt format
    
    * fix(auth): fix sign in and remove tests
    
    Removed the problematic tests for now.
    
    * fix: use constant instead of hardcoded
    
    * fix: ktfmt format the code
    
    * fix: ktfmt format the code
    
    * fix: fix wrong merge
    
    ---------
    
    Co-authored-by: Rémy <60262490+rBergCode@users.noreply.github.com>
    Co-authored-by: rBergCode <rberguerand@gmail.com>

--- app/src/androidTest/java/ch/hikemate/app/ui/auth/SignInScreenTest.kt
+++ app/src/androidTest/java/ch/hikemate/app/ui/auth/SignInScreenTest.kt
@@ -3,23 +3,41 @@ package ch.hikemate.app.ui.auth
 import androidx.compose.ui.test.assertHasClickAction
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertTextEquals
-import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
 import androidx.test.espresso.intent.Intents
-import ch.hikemate.app.MainActivity
+import ch.hikemate.app.model.authentication.AuthViewModel
 import ch.hikemate.app.ui.components.AppIcon
+import ch.hikemate.app.ui.navigation.NavigationActions
+import ch.hikemate.app.ui.navigation.TopLevelDestinations
+import com.google.firebase.auth.FirebaseUser
 import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import kotlinx.coroutines.flow.MutableStateFlow
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
 class SignInScreenTest : TestCase() {
-  @get:Rule val composeTestRule = createAndroidComposeRule<MainActivity>()
+  @get:Rule val composeTestRule = createComposeRule()
+
+  private lateinit var mockNavigationActions: NavigationActions
+  private lateinit var mockAuthViewModel: AuthViewModel
+  private val mockUserStateFlow = MutableStateFlow<FirebaseUser?>(null)
 
   @Before
   fun setUp() {
     Intents.init()
+
+    mockNavigationActions = mockk(relaxed = true)
+    mockAuthViewModel = mockk(relaxed = true)
+
+    // Replace the currentUser StateFlow with a mock, which is iniially null, so not signed in
+    every { mockAuthViewModel.currentUser } returns mockUserStateFlow
   }
 
   // Release Intents after each test
@@ -28,8 +46,15 @@ class SignInScreenTest : TestCase() {
     Intents.release()
   }
 
+  fun setupSignInScreen() {
+    composeTestRule.setContent {
+      SignInScreen(navigationActions = mockNavigationActions, authViewModel = mockAuthViewModel)
+    }
+  }
+
   @Test
   fun everythingIsOnScreen() {
+    setupSignInScreen()
     composeTestRule.onNodeWithTag(AppIcon.TEST_TAG).assertIsDisplayed()
 
     composeTestRule.onNodeWithTag(SignInScreen.TEST_TAG_TITLE).assertIsDisplayed()
@@ -40,5 +65,23 @@ class SignInScreenTest : TestCase() {
 
     composeTestRule.onNodeWithTag(SignInScreen.TEST_TAG_SIGN_IN_WITH_GOOGLE).assertIsDisplayed()
     composeTestRule.onNodeWithTag(SignInScreen.TEST_TAG_SIGN_IN_WITH_GOOGLE).assertHasClickAction()
+  }
+
+  @Test
+  fun loginButtonCallsAuthViewModel() {
+    setupSignInScreen()
+    composeTestRule.onNodeWithTag(SignInScreen.TEST_TAG_SIGN_IN_WITH_GOOGLE).performClick()
+
+    verify { mockAuthViewModel.signInWithGoogle(any(), any(), any()) }
+  }
+
+  @Test
+  fun whenUserIsSignedIn_navigatesToMap() {
+    val mockUser = mockk<FirebaseUser>(relaxed = true)
+    mockUserStateFlow.value = mockUser
+
+    setupSignInScreen()
+
+    verify { mockNavigationActions.navigateTo(TopLevelDestinations.MAP) }
   }
 }
