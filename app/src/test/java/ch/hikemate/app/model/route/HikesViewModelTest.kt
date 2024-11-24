fb4bf627aa0bb8bf3fa12e9d4dde43481463b295
Maël Imhof <contact@mael.im>
2024-11-24 10:15:44 +0100

    test(hikes): add tests for HikesViewModel.selectHike

--- app/src/test/java/ch/hikemate/app/model/route/HikesViewModelTest.kt
+++ app/src/test/java/ch/hikemate/app/model/route/HikesViewModelTest.kt
@@ -4,14 +4,22 @@ import ch.hikemate.app.model.elevation.ElevationService
 import ch.hikemate.app.model.route.saved.SavedHike
 import ch.hikemate.app.model.route.saved.SavedHikesRepository
 import io.mockk.coEvery
+import io.mockk.coVerify
 import io.mockk.mockk
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
 import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertNull
+import org.junit.Assert.assertTrue
+import org.junit.Assert.fail
 import org.junit.Before
+import org.junit.Test
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class HikesViewModelTest {
@@ -49,5 +57,52 @@ class HikesViewModelTest {
   private fun loadSavedHikes(savedHikes: List<SavedHike>, onSuccess: () -> Unit = {}, onFailure: () -> Unit = {})  {
     coEvery { savedHikesRepo.loadSavedHikes() } returns savedHikes
     hikesViewModel.loadSavedHikes(onSuccess, onFailure)
+  }
+
+  // ==========================================================================
+  // HikesViewModel.selectHike
+  // ==========================================================================
+
+  @Test
+  fun selectHikeFailsIfHikeNotFound() = runTest(dispatcher) {
+    // No hike is selected initially
+    assertNull(hikesViewModel.selectedHike.value)
+    val hikeId = "nonexistent"
+
+    // Select a non-existent hike
+    var onFailureCalled = false
+    hikesViewModel.selectHike(
+      hikeId = hikeId,
+      onSuccess = { fail("onSuccess should not have been called") },
+      onFailure = { onFailureCalled = true }
+    )
+
+    // The selected hike should still be null
+    assertNull(hikesViewModel.selectedHike.value)
+    // The appropriate callback should be called
+    assertTrue(onFailureCalled)
+  }
+
+  @Test
+  fun selectHikeSucceedsIfHikeFound() = runTest(dispatcher) {
+    // There needs to be at least one hike in the loaded list to select it
+    loadSavedHikes(singleSavedHike)
+    // No hike is selected initially
+    assertNull(hikesViewModel.selectedHike.value)
+    val hikeId = singleSavedHike[0].id
+
+    // Select the hike
+    var onSuccessCalled = false
+    hikesViewModel.selectHike(
+      hikeId = hikeId,
+      onSuccess = { onSuccessCalled = true },
+      onFailure = { fail("onFailure should not have been called") }
+    )
+
+    // The selected hike should now be the one that was selected
+    assertNotNull(hikesViewModel.selectedHike.value)
+    assertEquals(hikeId, hikesViewModel.selectedHike.value?.id)
+    // The appropriate callback should be called
+    assertTrue(onSuccessCalled)
   }
 }
