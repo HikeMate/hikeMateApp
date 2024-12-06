2cc3f23a999599d152520a2d658a5756ba6e7249
Maël Imhof <contact@mael.im>
2024-12-06 10:22:50 +0100

    test(run): fix run hike tests

--- app/src/androidTest/java/ch/hikemate/app/ui/map/RunHikeScreenTest.kt
+++ app/src/androidTest/java/ch/hikemate/app/ui/map/RunHikeScreenTest.kt
@@ -11,13 +11,17 @@ import androidx.compose.ui.test.performClick
 import ch.hikemate.app.model.elevation.ElevationService
 import ch.hikemate.app.model.profile.HikingLevel
 import ch.hikemate.app.model.profile.Profile
-import ch.hikemate.app.model.profile.ProfileRepository
-import ch.hikemate.app.model.profile.ProfileViewModel
 import ch.hikemate.app.model.route.Bounds
+import ch.hikemate.app.model.route.DetailedHike
+import ch.hikemate.app.model.route.Hike
+import ch.hikemate.app.model.route.HikeDifficulty
 import ch.hikemate.app.model.route.HikeRoute
 import ch.hikemate.app.model.route.HikeRoutesRepository
+import ch.hikemate.app.model.route.HikesViewModel
 import ch.hikemate.app.model.route.LatLong
-import ch.hikemate.app.model.route.ListOfHikeRoutesViewModel
+import ch.hikemate.app.model.route.saved.SavedHike
+import ch.hikemate.app.model.route.saved.SavedHikesRepository
+import ch.hikemate.app.model.route.toBoundingBox
 import ch.hikemate.app.ui.components.DetailRow
 import ch.hikemate.app.ui.navigation.NavigationActions
 import com.google.firebase.Timestamp
@@ -31,19 +35,92 @@ import org.mockito.Mockito.doNothing
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.`when`
 import org.mockito.kotlin.any
-import org.mockito.kotlin.eq
 import org.mockito.kotlin.verify
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class RunHikeScreenTest {
 
   @get:Rule val composeTestRule = createComposeRule()
 
   private lateinit var mockNavigationActions: NavigationActions
-  private lateinit var profileRepository: ProfileRepository
-  private lateinit var profileViewModel: ProfileViewModel
+  private lateinit var savedHikesRepository: SavedHikesRepository
   private lateinit var hikesRepository: HikeRoutesRepository
   private lateinit var elevationService: ElevationService
-  private lateinit var listOfHikeRoutesViewModel: ListOfHikeRoutesViewModel
+  private lateinit var hikesViewModel: HikesViewModel
+
+  private val hikeId = "1"
+  private val detailedHike =
+      DetailedHike(
+          id = hikeId,
+          color = Hike(hikeId, false, null, null).getColor(),
+          isSaved = false,
+          plannedDate = null,
+          name = "Sample Hike",
+          description =
+              "A scenic trail with breathtaking views of the Matterhorn and surrounding glaciers.",
+          bounds = Bounds(minLat = 45.9, minLon = 7.6, maxLat = 46.0, maxLon = 7.7),
+          waypoints = listOf(LatLong(45.9, 7.6), LatLong(45.95, 7.65), LatLong(46.0, 7.7)),
+          elevation = listOf(0.0, 10.0, 20.0, 30.0),
+          distance = 13.543077559212616,
+          elevationGain = 68.0,
+          estimatedTime = 169.3169307105514,
+          difficulty = HikeDifficulty.DIFFICULT,
+      )
+
+  private suspend fun setupCompleteScreenWithSelected(hike: DetailedHike) {
+    val asSavedHike = SavedHike(hike.id, hike.name ?: "", hike.plannedDate)
+
+    val hikeAsOsm =
+        HikeRoute(
+            id = hike.id,
+            bounds = hike.bounds,
+            ways = hike.waypoints,
+            name = hike.name,
+            description = hike.description,
+        )
+
+    // Make sure the saved status of the hike is set correctly
+    `when`(savedHikesRepository.loadSavedHikes())
+        .thenReturn(if (hike.isSaved) listOf(asSavedHike) else emptyList())
+
+    // Make sure that the hike is loaded from bounds when the view model gets it
+    `when`(hikesRepository.getRoutes(any(), any(), any())).thenAnswer {
+      val onSuccess = it.getArgument<(List<HikeRoute>) -> Unit>(1)
+      onSuccess(listOf(hikeAsOsm))
+    }
+
+    // Make sure the appropriate elevation profile is obtained when requested
+    `when`(elevationService.getElevation(any(), any(), any(), any())).thenAnswer {
+      val onSuccess = it.getArgument<(List<Double>) -> Unit>(2)
+      onSuccess(hike.elevation)
+    }
+
+    // Reset the view model
+    hikesViewModel =
+        HikesViewModel(
+            savedHikesRepository, hikesRepository, elevationService, UnconfinedTestDispatcher())
+
+    // Load the hike from OSM, as if the user had searched it on the map
+    hikesViewModel.loadHikesInBounds(detailedHike.bounds.toBoundingBox())
+
+    // Retrieve the hike's elevation data from the repository
+    hikesViewModel.retrieveElevationDataFor(hikeId)
+
+    // Compute the hike's details
+    hikesViewModel.computeDetailsFor(hikeId)
+
+    // Mark the hike as selected, to make sure it is the one displayed on the details screen
+    hikesViewModel.selectHike(hikeId)
+
+    // Set the test's content
+    composeTestRule.setContent {
+      RunHikeScreen(
+          hikesViewModel = hikesViewModel,
+          navigationActions = mockNavigationActions,
+      )
+    }
+  }
+
   private val route =
       HikeRoute(
           id = "1",
@@ -65,81 +142,39 @@ class RunHikeScreenTest {
   @Before
   fun setUp() {
     mockNavigationActions = mock(NavigationActions::class.java)
-    profileRepository = mock(ProfileRepository::class.java)
-    profileViewModel = ProfileViewModel(profileRepository)
+    savedHikesRepository = mock(SavedHikesRepository::class.java)
     hikesRepository = mock(HikeRoutesRepository::class.java)
     elevationService = mock(ElevationService::class.java)
-
-    listOfHikeRoutesViewModel =
-        ListOfHikeRoutesViewModel(hikesRepository, elevationService, UnconfinedTestDispatcher())
-
-    listOfHikeRoutesViewModel.selectRoute(route)
-
-    `when`(profileRepository.getProfileById(eq(profile.id), any(), any())).thenAnswer {
-      val onSuccess = it.getArgument<(Profile) -> Unit>(1)
-      onSuccess(profile)
-    }
-    profileViewModel.getProfileById(profile.id)
   }
 
   @Test
   fun runHikeScreen_displaysMap() = runTest {
-    composeTestRule.setContent {
-      RunHikeScreen(
-          listOfHikeRoutesViewModel = listOfHikeRoutesViewModel,
-          profileViewModel = profileViewModel,
-          navigationActions = mockNavigationActions,
-      )
-    }
+    setupCompleteScreenWithSelected(detailedHike)
     composeTestRule.onNodeWithTag(RunHikeScreen.TEST_TAG_MAP).assertIsDisplayed()
   }
 
   @Test
   fun runHikeScreen_displaysBackButton() = runTest {
-    composeTestRule.setContent {
-      RunHikeScreen(
-          listOfHikeRoutesViewModel = listOfHikeRoutesViewModel,
-          profileViewModel = profileViewModel,
-          navigationActions = mockNavigationActions,
-      )
-    }
+    setupCompleteScreenWithSelected(detailedHike)
     composeTestRule.onNodeWithTag(RunHikeScreen.TEST_TAG_BACK_BUTTON).assertIsDisplayed()
   }
 
   @Test
   fun runHikeScreen_displaysZoomButtons() = runTest {
-    composeTestRule.setContent {
-      RunHikeScreen(
-          listOfHikeRoutesViewModel = listOfHikeRoutesViewModel,
-          profileViewModel = profileViewModel,
-          navigationActions = mockNavigationActions,
-      )
-    }
+    setupCompleteScreenWithSelected(detailedHike)
     composeTestRule.onNodeWithTag(RunHikeScreen.TEST_TAG_ZOOM_BUTTONS).assertIsDisplayed()
   }
 
   @Test
   fun runHikeScreen_displaysBottomSheet() = runTest {
-    composeTestRule.setContent {
-      RunHikeScreen(
-          listOfHikeRoutesViewModel = listOfHikeRoutesViewModel,
-          profileViewModel = profileViewModel,
-          navigationActions = mockNavigationActions,
-      )
-    }
+    setupCompleteScreenWithSelected(detailedHike)
     composeTestRule.onNodeWithTag(RunHikeScreen.TEST_TAG_BOTTOM_SHEET).assertExists()
   }
 
   /** Test all details which do not change during the hike run. */
   @Test
   fun runHikeScreen_displaysStaticDetails() = runTest {
-    composeTestRule.setContent {
-      RunHikeScreen(
-          listOfHikeRoutesViewModel = listOfHikeRoutesViewModel,
-          profileViewModel = profileViewModel,
-          navigationActions = mockNavigationActions,
-      )
-    }
+    setupCompleteScreenWithSelected(detailedHike)
 
     composeTestRule.onNodeWithTag(RunHikeScreen.TEST_TAG_HIKE_NAME).assertIsDisplayed()
 
@@ -154,39 +189,21 @@ class RunHikeScreenTest {
 
   @Test
   fun runHikeScreen_displaysElevationGraph() = runTest {
-    composeTestRule.setContent {
-      RunHikeScreen(
-          listOfHikeRoutesViewModel = listOfHikeRoutesViewModel,
-          profileViewModel = profileViewModel,
-          navigationActions = mockNavigationActions,
-      )
-    }
+    setupCompleteScreenWithSelected(detailedHike)
 
     composeTestRule.onNodeWithTag(RunHikeScreen.TEST_TAG_ELEVATION_GRAPH).assertIsDisplayed()
   }
 
   @Test
   fun runHikeScreen_displaysStopHikeButton() = runTest {
-    composeTestRule.setContent {
-      RunHikeScreen(
-          listOfHikeRoutesViewModel = listOfHikeRoutesViewModel,
-          profileViewModel = profileViewModel,
-          navigationActions = mockNavigationActions,
-      )
-    }
+    setupCompleteScreenWithSelected(detailedHike)
 
     composeTestRule.onNodeWithTag(RunHikeScreen.TEST_TAG_STOP_HIKE_BUTTON).assertIsDisplayed()
   }
 
   @Test
   fun runHikeScreen_stopHikeButton_navigatesBack() = runTest {
-    composeTestRule.setContent {
-      RunHikeScreen(
-          listOfHikeRoutesViewModel = listOfHikeRoutesViewModel,
-          profileViewModel = profileViewModel,
-          navigationActions = mockNavigationActions,
-      )
-    }
+    setupCompleteScreenWithSelected(detailedHike)
     doNothing().`when`(mockNavigationActions).goBack()
 
     composeTestRule
@@ -205,13 +222,7 @@ class RunHikeScreenTest {
    */
   @Test
   fun runHikeScreen_displaysProgressIndicator() = runTest {
-    composeTestRule.setContent {
-      RunHikeScreen(
-          listOfHikeRoutesViewModel = listOfHikeRoutesViewModel,
-          profileViewModel = profileViewModel,
-          navigationActions = mockNavigationActions,
-      )
-    }
+    setupCompleteScreenWithSelected(detailedHike)
 
     composeTestRule
         .onNodeWithTag(RunHikeScreen.TEST_TAG_PROGRESS_TEXT)
