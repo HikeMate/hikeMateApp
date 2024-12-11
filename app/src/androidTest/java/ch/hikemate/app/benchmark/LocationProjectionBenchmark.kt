7c034c6c5a31b730e7fa0460141eb24991462a2c
mchac15 <matheo.chacon@epfl.ch>
2024-12-11 10:15:25 +0100

    fix: adapt Hike.kt and DetailedHike to the projection function

--- app/src/androidTest/java/ch/hikemate/app/benchmark/LocationProjectionBenchmark.kt
+++ app/src/androidTest/java/ch/hikemate/app/benchmark/LocationProjectionBenchmark.kt
@@ -3,9 +3,11 @@ package ch.hikemate.app.benchmark
 import androidx.benchmark.junit4.BenchmarkRule
 import androidx.benchmark.junit4.measureRepeated
 import ch.hikemate.app.model.route.Bounds
-import ch.hikemate.app.model.route.HikeRoute
+import ch.hikemate.app.model.route.DetailedHike
+import ch.hikemate.app.model.route.HikeDifficulty
 import ch.hikemate.app.model.route.LatLong
 import ch.hikemate.app.utils.LocationUtils.projectLocationOnHike
+import com.google.firebase.Timestamp
 import kotlin.random.Random
 import org.junit.Rule
 
@@ -13,7 +15,7 @@ import org.junit.Rule
 class LocationProjectionBenchmark {
   @get:Rule val benchmarkRule = BenchmarkRule()
 
-  private lateinit var longRoute: HikeRoute
+  private lateinit var longRoute: DetailedHike
   private lateinit var startPoint: LatLong
   private lateinit var midPoint: LatLong
   private lateinit var endPoint: LatLong
@@ -22,21 +24,33 @@ class LocationProjectionBenchmark {
   // @Before
   fun setup() {
     longRoute =
-        HikeRoute(
+        DetailedHike(
             id = "benchmark",
+            description = "Benchmark Hike",
             bounds = Bounds(45.0, 6.0, 47.0, 8.0),
-            // The most waypoints we observed for now were in the thousands. By going 100 times
-            // more, we ensure
-            // the benchmark provides relevant results for our use case.
-            ways = generateRealisticWaypoints(100000))
+            waypoints = generateRealisticWaypoints(100000),
+            elevation = List(100000) { Random.nextDouble(0.0, 1000.0) },
+            distance = 1000.0,
+            estimatedTime = 10.0,
+            elevationGain = 5000.0,
+            difficulty = HikeDifficulty.DIFFICULT,
+            color = 1,
+            isSaved = true,
+            plannedDate = Timestamp.now(),
+            name = "unnamed")
 
-    startPoint = LatLong(longRoute.ways.first().lat + 0.001, longRoute.ways.first().lon + 0.001)
+    startPoint =
+        LatLong(longRoute.waypoints.first().lat + 0.001, longRoute.waypoints.first().lon + 0.001)
 
-    val midIndex = longRoute.ways.size / 2
-    midPoint = LatLong(longRoute.ways[midIndex].lat + 0.001, longRoute.ways[midIndex].lon + 0.001)
+    val midIndex = longRoute.waypoints.size / 2
+    midPoint =
+        LatLong(
+            longRoute.waypoints[midIndex].lat + 0.001, longRoute.waypoints[midIndex].lon + 0.001)
 
-    endPoint = LatLong(longRoute.ways.last().lat + 0.001, longRoute.ways.last().lon + 0.001)
-    farPoint = LatLong(longRoute.ways[midIndex].lat + 1.0, longRoute.ways[midIndex].lon + 1.0)
+    endPoint =
+        LatLong(longRoute.waypoints.last().lat + 0.001, longRoute.waypoints.last().lon + 0.001)
+    farPoint =
+        LatLong(longRoute.waypoints[midIndex].lat + 1.0, longRoute.waypoints[midIndex].lon + 1.0)
   }
 
   // @Test
