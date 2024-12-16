05d32519173034c18d8c07d6d93550e901724e89
Wylwi <wylwi.fr@gmail.com>
2024-12-16 14:22:38 +0100

    fix: fix back buttons double clicks

--- app/src/main/java/ch/hikemate/app/ui/components/BackButton.kt
+++ app/src/main/java/ch/hikemate/app/ui/components/BackButton.kt
@@ -12,6 +12,11 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
@@ -34,8 +39,18 @@ fun BackButton(
     modifier: Modifier = Modifier,
     onClick: () -> Unit = { navigationActions.goBack() }
 ) {
+
+  var wantsToClick by remember { mutableStateOf(false) }
+
+  LaunchedEffect(wantsToClick) {
+    if (wantsToClick) {
+      onClick()
+      wantsToClick = false
+    }
+  }
+
   IconButton(
-      onClick = { onClick() },
+      onClick = { wantsToClick = true },
       modifier =
           modifier
               .testTag(BackButton.BACK_BUTTON_TEST_TAG)
